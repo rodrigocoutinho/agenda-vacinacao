d03a4e2f8e6b497f1511b2d6de635519cc22ab13
Rodrigo <cvc.francisco@gmail.com>
2023-02-05 19:30:41 -0300

    adjust jsonproperty

--- backend/src/main/java/br/ufg/inf/agendavacinacao/dto/request/AgendaRequest.java
+++ backend/src/main/java/br/ufg/inf/agendavacinacao/dto/request/AgendaRequest.java
@@ -3,19 +3,25 @@ package br.ufg.inf.agendavacinacao.dto.request;
 import br.ufg.inf.agendavacinacao.model.Situacao;
 import br.ufg.inf.agendavacinacao.model.Usuario;
 import br.ufg.inf.agendavacinacao.model.Vacina;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.sql.Date;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AgendaRequest {
-    private long id;
-
     private Vacina vacina;
     private Usuario usuario;
     private Date data;
-
+    
+    @JsonProperty("situacao")
     private Situacao situacao;
+
+    @JsonProperty("dataSituacao")
     private Date dataSituacao;
+
+    @JsonProperty("observecoes")
     private String observacoes;
 }
